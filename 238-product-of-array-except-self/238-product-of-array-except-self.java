89d8ef7c73c34d0b5cadb9d870915fa10af9956f
Mohamed Moussa <36421064+mohamedmoussa22@users.noreply.github.com>
2022-09-27 17:45:47 +0200

    Time: 3 ms (61.76%), Space: 57.6 MB (67.06%) - LeetHub

---
+++ 238-product-of-array-except-self/238-product-of-array-except-self.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] productExceptSelf(int[] arr) {
+        int n = arr.length;
+	    int i, temp = 1; 
+
+	    // Allocation of result array
+	    int result[] = new int[n]; 
+
+	    // Product of elements on left side excluding arr[i]
+	    for (i = 0; i < n; i++)  
+	    { 
+	      result[i] = temp; 
+	      temp *= arr[i]; 
+	    } 
+
+	    // Initializing temp to 1 for product on right side
+	    temp = 1; 
+
+	    // Product of elements on right side excluding arr[i] */
+	    for (i = n - 1; i >= 0; i--)  
+	    { 
+	      result[i] *= temp; 
+	      temp *= arr[i]; 
+	    } 
+	    return result; 
+    }
+}
